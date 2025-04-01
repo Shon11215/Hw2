ece75a9c88bd8edc01e112635cce03b2580fff3a
Shon11215 <shon11215@gmail.com>
2025-04-01 18:41:22 +0300

    Add project files.

---
+++ Hw2/ClothingItem.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw2
+{
+    internal class ClothingItem
+    {
+        uint _uint;
+        int cost, usage_status;
+        string[] seasons = new string[4];
+        string name, color, size, type, brand;
+        bool is_favorite, is_casual;
+
+        public ClothingItem(uint _uint, string color, string name, string[] seasson, string is_favorite, int usage_status, string type, string brand, int cost) : this(name, null) {
+            SetCost(cost);
+            UsageStatus(usage_status);
+            SetColor(color);
+            SetSize(size);
+            this._uint = _uint;
+            this.seasons = seasson;
+            if (is_favorite.ToLower() == "yes" || is_favorite.ToLower() == "true" || is_favorite == "1") {
+                this.is_favorite = true;
+            }
+
+            this.brand = brand;
+            this.type = type;
+
+
+        }
+
+        public ClothingItem() { }
+        public ClothingItem(string name, string is_casual) {
+            this.name = name;
+            SetIsCasual(is_casual);
+        }
+        public void Print() {
+            Console.WriteLine($"The name of the item is: {this.name}\nColor: {this.color}\nSeason: \nFavorite?: {this.is_favorite}");
+            Console.WriteLine($"Usage Status: {this.usage_status}\nType: {this.type}\nBrand: {this.brand}\nCost: {this.cost}\nCasual?: {this.is_casual}");
+        }
+
+        public void SetCost(int cost) {
+            while (cost <= 0) {
+                Console.Write("Please enter a postive price tag: ");
+                cost = int.Parse(Console.ReadLine());
+            }
+            this.cost = cost;
+        }
+        public void UsageStatus(int usage_status) {
+            while (usage_status < 1 && usage_status > 3) {
+                Console.Write("Please enter a number between 1-3 for the usage rate: ");
+                usage_status = int.Parse(Console.ReadLine());
+            }
+            this.usage_status = usage_status;
+
+        }
+        public void SetColor(string color) {
+            while (true) {
+                if (color[0] != '#') {
+                    Console.WriteLine("please start the color with # : ");
+                }
+                else if (color.Length != 7) {
+                    Console.WriteLine("please enter a 7 digits : ");
+                }
+                else if (!IsValidColor(color)) {
+                    Console.WriteLine("Please enter only numbers or a-f chars");
+                }
+                else {
+                    this.color = color;
+                    break;
+
+                }
+                color = Console.ReadLine();
+            }
+
+        }
+
+
+        static bool IsValidColor(string color) {
+            for (int i = 1; i < color.Length; i++) {
+                if (!char.IsDigit(color[i]) && (color[i] < 'a' || color[i] > 'f')) return false;
+            }
+            return true;
+        }
+        public void SetSize(string size) {
+            string[] sizes = new string[] { "S", "M", "L", "XL", "XXL", "OS" };
+            bool is_size = false;
+            while (!is_size) {
+                foreach (string curr_size in sizes) {
+                    if (curr_size == size.ToUpper()) {
+                        this.size = size;
+                        is_size = true;
+                        break;
+                    }
+                }
+                if (!is_size) {
+                    Console.Write("please enter a new size: ");
+                    size = Console.ReadLine();
+                }
+            }
+        }
+        public void SetIsCasual(string is_casual) {
+            if (is_casual.ToLower() == "yes" || is_casual.ToLower() == "true" || is_casual == "1") {
+                this.is_casual = true;
+            }
+        }
+        public bool GetIsCasual() {
+            return is_casual;
+        }
+
+    }
+}
---
+++ Hw2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw2
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            ClothingItem[] clothes = new ClothingItem[200];
+            uint _uint = 1000;
+            string index = null;
+            do {
+                Console.WriteLine("Hey what do you want to do?\n\na - Add a new Clothing Item\nb - See all your wardrobe\nc - Exit\n");
+                Console.Write("Please enter your pick: ");
+                index = Console.ReadLine();
+                switch (index.ToLower()) {
+                    case "a":
+                        initCloathingItem();
+                        break;
+                    case "b":
+                        break;
+                    case "c":
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (index.ToLower() != "c");
+        }
+
+        static void initCloathingItem() {
+            string[] season_list = new string[4] { "summer", "spring", "winter", "fall" };
+            string[] seasons_to_pass = new string[4];
+            int[] season_index = new int[4];
+            int season_counter;
+
+            Console.Write("Please enter the name of the item: ");
+            string name = Console.ReadLine();
+
+            Console.Write("\nPlease enter the color code of the item: ");
+            string color = Console.ReadLine();
+
+            Console.Write("\n1) none\n2) average\n3) high\nPlease enter the amout of usage the item has: ");
+            int usage_status = int.Parse(Console.ReadLine());
+
+            Console.Write("\nPlease enter the cost of the item: ");
+            int cost = int.Parse(Console.ReadLine());
+
+            do {
+                Console.Write("\nPlease enter the amount of seasons your item fits between 1-4: ");
+                season_counter = int.Parse(Console.ReadLine());
+            } while (season_counter < 1 || season_counter > 4);
+
+            Console.Write("\n1) summer\n2) spring\n3) winter\n4) fall\nPlease enter the number for the following seassons this cloathing is attenable for:");
+
+            for (int i = 0; i < season_counter; i++) {
+                int curr_season = int.Parse(Console.ReadLine());
+                if (season_index.Contains(curr_season)) {
+                    Console.WriteLine($"{season_list[curr_season - 1]} Is already in please enter a diff season");
+                    i--;
+                    continue;
+                }
+                season_index[i] = curr_season;
+                seasons_to_pass[i] = season_list[curr_season];
+            }
+            Console.Write("\nPlease enter True or Flase for wether the item is favorite or not: ");
+            string is_favorite = (Console.ReadLine());
+            Console.Write("\nPlease enter the type of your item: ");
+            string type = (Console.ReadLine());
+            Console.Write("\nPlease enter the brand name: ");
+            string brand = (Console.ReadLine());
+
+        }
+    }
+}
